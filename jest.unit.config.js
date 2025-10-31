58db9f9d911465931837a56fb0701248199363e1
Nahalil <nahalilmuchtar2@gmail.com>
2025-11-01 00:17:50 +0800

    ressolved integration test setup

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFilesAfterEnv: ["./src/tests/mocks/prisma.ts"],
+  setupFilesAfterEnv: ["./src/tests/mockPrisma.ts"],
   testMatch: ["**/**/*.unit.test.ts"],
 };
