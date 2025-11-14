2f0bd289284ac13d389f9f788f1100f0a03a09c9
Nahalil <nahalilmuchtar2@gmail.com>
2025-11-14 13:40:27 +0800

    set courier in create order schema as optional

--- src/schemas/orderSchemas.ts
+++ src/schemas/orderSchemas.ts
@@ -4,7 +4,7 @@ export const createOrderSchema = z.object({
   body: z.object({
     userId: z.string().min(1, "User ID wajib ada"),
     addressId: z.string().optional(),
-    courier: z.string().min(1, "Nama kurir wajib ada"),
+    courier: z.string().optional(),
     fullfillmentType: z.string().min(1, "Pemenuhan Order Wajib Ada"),
     paymentMethod: z.string().min(1, "Metode pembayaran wajib ada"),
     totalAmount: z.number().min(0, "Harga total wajib ada"),
